10ead41f3114c6ec5f44bc11d369f89b45624d6b
Søs Wind <s_swind@hotmail.com>
2025-03-10 20:01:33 +0100

    Error with link fixed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import image from "/src/assets/profil.png";
-import Link from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
